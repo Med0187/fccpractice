5332ee71cfaaeb8cc993e386d85595304f795bd7
Medardo Diaz <101226958+Med0187@users.noreply.github.com>
2022-05-16 22:37:25 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,6 +6,8 @@ app.use(express.static(__dirname+"/public/style.css"));
 app.get("/", function(req,res) {
     res.sendFile(__dirname+"/views/index.html");
 });
+console.log(__dirname+"/public/style.css");
+console.log(__dirname+"/views/index.html");
 
 
 
